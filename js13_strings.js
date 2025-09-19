e93cfe0574fd6f01b5cc0e572b65470859368eb2
Mahtab <skywatcher181@gmail.com>
2025-09-19 16:33:54 +0600

    third

---
+++ js13_strings.js
@@ -0,0 +1,27 @@
+let name = "mahtab";
+console.log(name);
+console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+let friend = "eashan";
+console.log(friend);
+console.log(friend.length);
+
+//template literals // `` and ${}
+let boy1 = "rafik";
+let boy2 = "karim";
+//rafik is a friend of karim
+let sentence = `${boy1} is a friend of ${boy2}.`;
+console.log(sentence);
+
+//Escape sequence
+let fruit = "bana'na";
+console.log(fruit);
+console.log(fruit.length);
+let dow = "moo\rnoo";
+let cow = "moo\tnoo";
+let wow = "moo\nnoo";
+console.log(dow);
+console.log(cow);
+console.log(wow);
---
+++ js14_strings_methods.js
@@ -0,0 +1,10 @@
+let naam = 'mahta"b';
+console.log(naam.length);
+console.log(naam.toUpperCase());
+console.log(naam.toLowerCase());
+console.log(naam.slice(2, 4));
+console.log(naam.slice(2));
+console.log(naam.replace("mah", "Mah"));
+
+let friend = "eashan";
+console.log(naam.concat(" is a friend of ", friend, ",Ok?"));
